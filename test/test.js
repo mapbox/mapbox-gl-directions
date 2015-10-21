9a61d78927653dc675b2405d38ec6f92b3cee43e
tristen <tristen.brown@gmail.com>
2015-10-21 18:00:10 -0400

    More test setup

--- test/test.js
+++ test/test.js
@@ -1,2 +1,10 @@
 const test = require('tape');
-const Directions = require('../');
+// const Directions = require('../');
+
+// close the smokestack window once tests are complete
+test('shutdown', function(t) {
+  t.end();
+  setTimeout(function() {
+    window.close();
+  });
+});
