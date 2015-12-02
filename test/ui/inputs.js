93abd722e75e9b1ff9d471b955804530030bd51c
tristen <tristen.brown@gmail.com>
2015-12-02 10:48:28 -0500

    test profile event

--- test/ui/inputs.js
+++ test/ui/inputs.js
@@ -15,33 +15,46 @@ test('Directions#inputControl', tt => {
 
   tt.test('origin', t => {
     setup();
+    t.plan(2);
     var inputOrigin = container.querySelector('.js-origin');
     var inputOriginClear = container.querySelector('.js-origin-clear');
     inputOrigin.addEventListener('change', once(() => {
       t.ok(inputOrigin.value, 'value populates in origin');
       t.ok(inputOriginClear.classList.contains('active'), 'clear link is active');
-      t.end();
     }));
     directions.setOrigin([-79, 43]);
   });
 
   tt.test('destination', t => {
     setup();
+    t.plan(2);
     var inputDestination = container.querySelector('.js-destination');
     var inputDestinationClear = container.querySelector('.js-destination-clear');
     inputDestination.addEventListener('change', once(() => {
       t.ok(inputDestination.value, 'value populates in destination');
       t.ok(inputDestinationClear.classList.contains('active'), 'clear link is active');
-      t.end();
     }));
     directions.setDestination([-78, 42]);
   });
 
   tt.test('profiles', t => {
     setup({ profile: 'cycling' });
-    t.equal(container.querySelector('#mapbox-directions-profile-driving').checked, false, 'default driving profile should is false');
-    t.equal(container.querySelector('#mapbox-directions-profile-cycling').checked, true, 'cycling profile is active');
-    t.end();
+    t.plan(3);
+
+    var changeEvent = document.createEvent('HTMLEvents');
+    changeEvent.initEvent('change', true, false);
+
+    var drivingEl = container.querySelector('#mapbox-directions-profile-driving');
+    var cyclingEl = container.querySelector('#mapbox-directions-profile-cycling');
+
+    t.equal(drivingEl.checked, false, 'default driving profile should is false');
+    t.equal(cyclingEl.checked, true, 'cycling profile is active');
+
+    directions.on('directions.profile', once((e) => {
+      t.equal(e.profile, 'driving', 'driving profile is set and event emitted');
+    }));
+
+    drivingEl.dispatchEvent(changeEvent);
   });
 
   tt.end();
